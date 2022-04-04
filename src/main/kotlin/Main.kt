6ca2dac814299800c549aee94b2dd3448b69231f
stevenwhy <stevenyildirim@gmail.com>
2022-04-03 20:59:03 -0400

    find smallest char that is greater than or equal to target in ascending list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,8 +10,29 @@ fun main() {
     println("Find ceiling: ${findCeiling(listOf(1,3,8,10,14),12)}")
     println("Find ceiling: ${findCeiling(listOf(4,6,10),17)}")
     println("Find ceiling: ${findCeiling(listOf(4,6,10),-1)}")
+
+    println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'f')}")
+    println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'b')}")
+    println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'m')}")
+    println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'h')}")
 }
 
+fun nextSmallestLetter(list: List<Char>, target: Char): Char {
+    if(target >= list.last() || target < list.first()) return list.first()
+
+    var start = 0
+    var end = list.size - 1
+    while(start <= end) {
+        val middle = start + (end-start)/2
+
+        if(target >= list[middle]) {
+            start = middle + 1
+        } else if (target < list[middle]) {
+            end = middle - 1
+        }
+    }
+    return list[start]
+}
 // given asc list, find smallest number >= target, return index
 fun findCeiling(list: List<Int>, target: Int): Int {
     var start = 0
