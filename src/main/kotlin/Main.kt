413f114abe54affe47c3502a49b378105ea94550
stevenwhy <stevenyildirim@gmail.com>
2022-04-03 21:12:27 -0400

    find smallest char that is greater than or equal to target in ascending list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,8 +15,44 @@ fun main() {
     println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'b')}")
     println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'m')}")
     println("Find Next Smallest Letter: ${nextSmallestLetter(listOf('a', 'c', 'f', 'h'),'h')}")
+
+    println("Find range: ${findRange(listOf(4, 6, 6, 6, 9),6)}")
+    println("Find range: ${findRange(listOf(1, 3, 8, 10, 15),10)}")
+    println("Find range: ${findRange(listOf(1, 3, 8, 10, 15),12)}")
 }
 
+fun findRange(list: List<Int>, target: Int): Pair<Int,Int> {
+    if(target > list.last() || target < list.first()) return Pair(-1,-1)
+
+    return Pair(findLowerRange(list,target), findUpperRange(list,target))
+
+}
+private fun findLowerRange(list: List<Int>, target: Int): Int {
+    var start = 0
+    var end = list.size - 1
+    while(start <= end) {
+        val middle = start + (end-start)/2
+
+        if(target <= list[middle]) {
+            end = middle - 1
+        } else start = middle + 1
+    }
+    if(list[start] != target) return -1
+    return start
+}
+private fun findUpperRange(list: List<Int>, target: Int): Int {
+    var start = 0
+    var end = list.size - 1
+    while(start <= end) {
+        val middle = start + (end-start)/2
+        if(target >= list[middle]) {
+            start = middle + 1
+        } else end = middle - 1
+
+    }
+    if(list[end] != target) return -1
+    return end
+}
 fun nextSmallestLetter(list: List<Char>, target: Char): Char {
     if(target >= list.last() || target < list.first()) return list.first()
 
